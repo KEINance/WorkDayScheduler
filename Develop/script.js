1539a7af5e2f0ce2bbda9c9af39cdff6d1c48bed
KEINance <keinance2@gmail.com>
2023-06-25 14:11:00 -0400

    added colour block changer

--- Develop/script.js
+++ Develop/script.js
@@ -1,22 +1,54 @@
 var hourBlock = document.querySelector('.container');
 
 
-$(document).ready(function () {
 // show current date and time
 var todaysDate = document.querySelector('#currentDay');
-$('#today').text(today.format("llll")); 
 
-todaysDate.textContent = $('#today');
+todaysDate.textContent = $('#today').text(today.format("llll"));
 
 
 // save btn clicker functionality for local storage
 $('.saveBtn').on('click', function() {
   var userText = $(this).siblings('.description').val();
   var time = $('this').parent().attr('id');
-
+  
   localStorage.setTime(userText, time);
 });
 
+// tracker for hours and past, present and future sorter for colour blocks
+function timeSorter() {
+  var currentTime = today.hour();
+
+  //loop hour blocks
+  $('.time-block').each(function() {
+      var timeBlock = parseInt($(this).attr('.id').split('hour')[1]);
+
+
+// colour block changes based on current time app is being used
+      if (timeBlock < currentTime) {
+        $(this).addClass('past');
+      }
+      else if (timeBlock === currentTime) {
+        $(this).removeClass('past');
+        $(this).removeClass('future');
+        $(this).addClass('present');
+      }
+      else {
+        $(this).removeClass('past');
+        $(this).removeClass('present');
+        $(this).addClass('future');
+      }
+
+
+
+
+
+
+  })
+}
+
+
+$(document).ready(function () {
 
   
   
