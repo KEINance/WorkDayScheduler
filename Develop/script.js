fe4edbdd2f96d353ccd1d8b17d80dd2dba6343fe
KEINance <keinance2@gmail.com>
2023-06-25 16:27:34 -0400

    final edits, local storage change

--- Develop/script.js
+++ Develop/script.js
@@ -3,38 +3,36 @@ var todaysDate = document.querySelector('#currentDay');
 
 $(document).ready(function () {
 
-  // show current date and time
+// show current date and time
 var hour = dayjs().format('dddd, MMMM D, YYYY h:mm');
 todaysDate.textContent = hour;
 
 // save btn clicker functionality for local storage
-$('.saveBtn').on('click', function() {
+$('.saveBtn').on('click', function () {
   var userText = $(this).siblings('.description').val();
   var time = $(this).parent().attr('id');
   
-  localStorage.setItem(userText, time);
+  localStorage.setItem(time, userText);
 });
 
 // tracker for hours and past, present and future sorter for colour blocks
 function timeSorter() {
-  var currentTime = dayjs.hour();
+  var currentTime = dayjs().hour();
 
-  //loop hour blocks
-  $('.time-block').each(function() {
-      var timeBlock = parseInt($(this).attr('.id').split('hour')[1]);
+//loop hour blocks
+$('.time-block').each(function() {
+  var timeBlock = parseInt($(this).attr('id').split('hour')[1]);
 
 // colour block changes based on current time app is being used
-      if (timeBlock < currentTime) {
+if (timeBlock < currentTime) {
         $(this).removeClass('future');
         $(this).removeClass('present');
         $(this).addClass('past');
-      }
-      else if (timeBlock === currentTime) {
+} else if (timeBlock === currentTime) {
         $(this).removeClass('past');
         $(this).removeClass('future');
         $(this).addClass('present');
-      }
-      else {
+} else {
         $(this).removeClass('past');
         $(this).removeClass('present');
         $(this).addClass('future');
@@ -45,11 +43,11 @@ $('#hour9 .description').val(localStorage.getItem('hour9'));
 $('#hour10 .description').val(localStorage.getItem('hour10'));
 $('#hour11 .description').val(localStorage.getItem('hour11'));
 $('#hour12 .description').val(localStorage.getItem('hour12'));
-$('#hour1 .description').val(localStorage.getItem('hour1'));
-$('#hour2 .description').val(localStorage.getItem('hour2'));
-$('#hour3 .description').val(localStorage.getItem('hour3'));
-$('#hour4 .description').val(localStorage.getItem('hour4'));
-$('#hour5 .description').val(localStorage.getItem('hour5'));
+$('#hour13 .description').val(localStorage.getItem('hour13'));
+$('#hour14 .description').val(localStorage.getItem('hour14'));
+$('#hour15 .description').val(localStorage.getItem('hour15'));
+$('#hour16 .description').val(localStorage.getItem('hour16'));
+$('#hour17 .description').val(localStorage.getItem('hour17'));
 
 
 timeSorter();
